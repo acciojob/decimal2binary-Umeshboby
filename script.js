f5eee00b8f14ca6d66434cc6ff5aa4f8678f35e1
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-10-23 17:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let result="";
+	while (num!=0) {
+		result=(num%2)+result;
+		num=Math.floor(num/2);
+	}
+  return result;
 }
 
 window.decimalToBinary = decimalToBinary;
